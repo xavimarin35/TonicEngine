1e2086401b273fef5f6ff7b4aceb970fcac5b5a7
xavimarin35 <36265010+xavimarin35@users.noreply.github.com>
2020-10-03 11:58:15 +0200

    base code (cleaning WIP)

---
+++ TonicEngine/ModuleSceneIntro.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "Module.h"
+#include "p2DynArray.h"
+#include "Globals.h"
+#include "Primitive.h"
+
+#define MAX_SNAKE 2
+
+struct PhysBody3D;
+struct PhysMotor3D;
+
+class ModuleSceneIntro : public Module
+{
+public:
+	ModuleSceneIntro(Application* app, bool start_enabled = true);
+	~ModuleSceneIntro();
+
+	bool Start();
+	update_status Update(float dt);
+	bool CleanUp();
+
+	void OnCollision(PhysBody3D* body1, PhysBody3D* body2);
+
+public:
+	/*
+	PhysBody3D* pb_snake[MAX_SNAKE];
+	Sphere s_snake[MAX_SNAKE];
+
+	PhysBody3D* pb_snake2[MAX_SNAKE];
+	Sphere s_snake2[MAX_SNAKE];
+	*/
+
+	PhysBody3D* pb_chassis;
+	Cube p_chassis;
+
+	PhysBody3D* pb_wheel;
+	Cylinder p_wheel;
+
+	PhysBody3D* pb_wheel2;
+	Cylinder p_wheel2;
+
+	PhysMotor3D* left_wheel;
+	PhysMotor3D* right_wheel;
+};
