50a26a9b8bc34a3ffac61433e382ebe0d98e8a8e
xavimarin35 <36265010+xavimarin35@users.noreply.github.com>
2020-10-12 14:53:33 +0200

    error fixed, panels system works well!! :new_moon_with_face:

--- TonicEngine/PanelConfiguration.cpp
+++ TonicEngine/PanelConfiguration.cpp
@@ -29,33 +29,33 @@ bool PanelConfiguration::Draw()
 {
 	if (App->gui->configPanel_visible)
 	{
-		//ImVec4 clear_color = ImVec4(0.15f, 0.15f, 0.15f, 0.50f);
-		//static float f = 0.0f;
-		//static int counter = 0;
+		/*ImVec4 clear_color = ImVec4(0.15f, 0.15f, 0.15f, 0.50f);
+		static float f = 0.0f;
+		static int counter = 0;*/
 
-		//ImGui::Begin("Custom Window");                          // Create a window called "Hello, world!" and append into it.
+		ImGui::Begin("Custom Window");                          // Create a window called "Hello, world!" and append into it.
 
-		//ImGui::Text("Welcome to 3D TonicEngine");               // Display some text (you can use a format strings too)
-		////ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-		////ImGui::Checkbox("Another Window", &show_another_window);
+		ImGui::Text("Welcome to 3D TonicEngine");               // Display some text (you can use a format strings too)
+		//ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+		//ImGui::Checkbox("Another Window", &show_another_window);
 
-		////ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-		////ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+		//ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+		//ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
-		////if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-		////	counter++;
+		//if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+		//	counter++;
+		//ImGui::SameLine();
+		//ImGui::Text("counter = %d", counter);
+
+		//// Random Number Generator
+		////if (ImGui::Button("Random"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+		////	App->gui->rand = GenerateRandomBetween(100);
 		////ImGui::SameLine();
-		////ImGui::Text("counter = %d", counter);
-
-		////// Random Number Generator
-		//////if (ImGui::Button("Random"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-		//////	App->gui->rand = GenerateRandomBetween(100);
-		//////ImGui::SameLine();
-		//////ImGui::Text("%d", rand);
+		////ImGui::Text("%d", rand);
 
 
-		//ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		//ImGui::End();
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::End();
 	}
 
 	return true;
