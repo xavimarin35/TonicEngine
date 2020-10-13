2378da0f14edb5d69c0563ed52ea5c911ad59010
xavimarin35 <36265010+xavimarin35@users.noreply.github.com>
2020-10-14 01:14:39 +0200

    minor on about

--- TonicEngine/PanelAbout.cpp
+++ TonicEngine/PanelAbout.cpp
@@ -33,15 +33,15 @@ bool PanelAbout::Draw()
 			ImGui::Text("%s ", SDL_GetWindowTitle(App->window->window));
 			ImGui::Separator();
 
-			ImGui::Text("3D Game Engine developed in C & C++ during our Game Design and Development Bachelor's Degree.");
+			ImGui::Text("This is a 3D Game Engine developed during our Game Design and Development Bachelor's Degree.");
 			ImGui::Text("By Pol Casau and Xavi Marin\n");
 
 			if (ImGui::Button("GitHub"))
-				ShellExecuteA(NULL, "open", "https://github.com/xavimarin35/TonicEngine", NULL, NULL, SW_SHOWNORMAL);
+				App->RequestBrowser("https://github.com/xavimarin35/TonicEngine"); 
 
-			//ImGui::Separator();
+			ImGui::Separator();
 
-			ImGui::Text("\n3rd Party Libraries used:");
+			ImGui::Text("3rd Party Libraries used:");
 			if (ImGui::Selectable("OpenGL", false, 0, { 50, 13 }))
 				App->RequestBrowser("https://www.opengl.org/"); ImGui::SameLine();
 			if (ImGui::Selectable("Glew", false, 0, { 35, 13 }))
@@ -55,25 +55,25 @@ bool PanelAbout::Draw()
 			if (ImGui::Selectable("MathGeoLib", false, 0, { 90, 13 }))
 				App->RequestBrowser("https://github.com/juj/MathGeoLib");
 
-			ImGui::Text("\nMIT License");
+			ImGui::Separator();
+
+			ImGui::Text("MIT License");
 			ImGui::Text("Copyright (c) 2020 Xavi Marin Sola and Pol Casau Civit");
-			ImGui::Text("Permission is hereby granted, free of charge, to any person obtaining a copy");
-			ImGui::Text("of this software and associated documentation files (the 'Software'), to deal");
-			ImGui::Text("in the Software without restriction, including without limitation the rights");
-			ImGui::Text("to use, copy, modify, merge, publish, distribute, sublicense, and / or sell");
-			ImGui::Text("copies of the Software, and to permit persons to whom the Software is");
-			ImGui::Text("furnished to do so, subject to the following conditions :");
+			ImGui::Text("Permission is hereby granted, free of charge, to any person obtaining a copy of this");
+			ImGui::Text("software and associated documentation files (the 'Software'), to deal in the Software without");
+			ImGui::Text("restriction, including without limitation the rights to use, copy, modify, merge, publish,");
+			ImGui::Text("distribute, sublicense, and / or sell copies of the Software, and to permit  persons");
+			ImGui::Text("to whom the Software is furnished to do so, subject to the following conditions :");
 
-			ImGui::Text("\nThe above copyright notice and this permission notice shall be included in all");
-			ImGui::Text("copies or substantial portions of the Software.");
+			ImGui::Text("\nThe above copyright notice and this permission notice shall be included in all copies");
+			ImGui::Text("or substantial portions of the Software.");
 
-			ImGui::Text("\nTHE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR");
-			ImGui::Text("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,");
-			ImGui::Text("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE");
-			ImGui::Text("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER");
-			ImGui::Text("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,");
-			ImGui::Text("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE");
-			ImGui::Text("SOFTWARE.");
+			ImGui::Text("\nTHE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,");
+			ImGui::Text("INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A");
+			ImGui::Text("PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS");
+			ImGui::Text("BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,");
+			ImGui::Text("TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE");
+			ImGui::Text("USE OR OTHER DEALINGS IN THE SOFTWARE.");
 
 			ImGui::End();
 		}
